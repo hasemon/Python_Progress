5e8b99f22bc89ab78697723d1a33b1bdc070010c
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-09-19 14:20:02 +0600

    Declaration of variables and its usage

---
+++ Day 1/task1.py
@@ -0,0 +1,7 @@
+name = input('Enter name: ')
+age = input('Enter age: ')
+location = input('Enter location: ')
+
+print('My name is', name, ',')
+print('I am ', age, 'years old,')
+print('I am from', location, '.')
---
+++ Day 1/task2.py
@@ -0,0 +1,12 @@
+num_1 = int(input('Enter first number: '))
+num_2 = int(input('Enter second number: '))
+total_sum = num_1 + num_2
+product = num_1 * num_2
+average = total_sum / 2
+
+print('first number is:', num_1)
+print('second number is:', num_2)
+print('sum of two number is :', total_sum)
+print('product of two numbers :', product)
+print('Average is ', average)
+
