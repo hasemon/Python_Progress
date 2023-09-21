275620413ba1c8338615d1bb06e980c2db22d443
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-09-21 10:47:50 +0600

    Update hourAndMinutes.py

--- Day 2/hourAndMinutes.py
+++ Day 2/hourAndMinutes.py
@@ -3,4 +3,4 @@ total_minute = int(input('Enter Total minute: '))
 hour = total_minute // 60
 minutes = total_minute % 60
 
-print('Total ', hour,'hour', 'and', minutes44, 'minutes')
+print('Total ', hour,'hour', 'and', minutes, 'minutes')
