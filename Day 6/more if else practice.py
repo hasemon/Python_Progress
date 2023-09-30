9671a0d8635c161a9d008d623105c2a4db0d14c4
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-09-30 08:14:41 +0600

    Day 6 Added

---
+++ Day 6/more if else practice.py
@@ -0,0 +1,7 @@
+print('if elif else - Exercise')
+# Create a calculator which handles +,-,*,/ and outputs answer based on the mode/ operator used
+# Hint: use 3 separate inputs
+# Bonus: Extend functionality with extra mode, so it also does Celsius to Fahrenheit conversion
+# formula is: temp in C*9/5 + 32 = temp in f
+
+firstNumber = int(input())
