6279ab258549838ff2ec38bdbe7b550774564e2f
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-10-01 23:02:38 +0600

    All 3 solution added ICT xm questions

--- Day 7/task_6.py
+++ Day 7/task_6.py
@@ -1,1 +1,13 @@
-# incomplete
+# write a program to read marks of n students and find their average mark.
+
+N = int(input('Enter the number of students: '))
+
+sumOfMarks = 0
+i = 1
+while i <= N:
+    marks = int(input('Enter marks: '))
+    sumOfMarks += marks
+    i += 1
+
+average = sumOfMarks / N
+print(f'Their average is: {average} marks')
--- Lecture 3/solution_3.py
+++ Lecture 3/solution_3.py
@@ -5,4 +5,4 @@ number = int(input('Enter the number to find all odd number: '))
 i = 1
 while i <= number:
     print(i)
-    i += 1
+    i += 2
