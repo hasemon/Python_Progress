84a565f35c4557d4811edb0804cf73066fbfdb4b
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-10-16 12:15:37 +0600

    day 4 updated

---
+++ Day 8/python_lists.py
@@ -0,0 +1,16 @@
+list1 = ['physics', 'chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5, 6, 7]
+
+# accessing values
+# print(list1)
+# print(list1[0])
+# print(list2[1:5])
+# print(list2[1:])
+
+# updating values
+my_list = ['physics', 'chemistry', 1997, 2000]
+print(my_list[2])
+my_list[2] = 2023
+print(my_list[2])
+list[2:4] = [1999, 2002]
+print(my_list)
---
+++ Lecture 4/solution_1.py
@@ -0,0 +1,1 @@
+# 1. Write a program to sum the numbers in a list using for loop.
