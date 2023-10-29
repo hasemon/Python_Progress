2b28c6017a94670a758bffdd538c211939778bf4
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-10-30 00:18:07 +0600

    Minor changes

--- Day 12/linear search q1.py
+++ Day 12/linear search q1.py
@@ -4,12 +4,11 @@ search = input('Enter fruit name: ')
 found = False
 location = 0
 
-for fruit in fruits:
-    if fruit == search:
+for item in fruits:
+    if item == search:
         found = True
         break
-    else:
-        location += 1
+    location += 1
 if found:
     print(search, 'is found at Index:', location)
 else:
--- Day 12/linear search q2.py
+++ Day 12/linear search q2.py
@@ -1,14 +1,14 @@
 fruits = ['Apple', 'Pineapple', 'Banana', 'Guava', 'Lichi', 'Banana', 'Mango', 'Apple', 'Banana', 'Orange']
 
-search_fruit = input('Enter fruit name: ')
+search = input('Enter fruit name: ')
 found = False
 count = 0
 
 for fruit in fruits:
-    if fruit == search_fruit:
+    if fruit == search:
         found = True
         count += 1
 if found:
-    print(search_fruit, 'is found', count, 'times')
+    print(search, 'is found', count, 'times')
 else:
     print('There is no such fruit in the list')
