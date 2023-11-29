bfc686ceca5631740abdd61121bc2202d7d1267a
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-11-29 06:37:38 +0600

    added exception handling

--- Day 18/exception handling/exception_handling_class.py
+++ Day 18/exception handling/exception_handling_class.py
@@ -1,22 +1,28 @@
 import os
 
-try:
-    f = open('demo.txt', 'r')
-    content = f.read()
-    print(content)
-    f.close()
-except:
-    print('Create the file first.')
+# over writing in a file or creating
+f = open('demo.txt', 'w')
+f.write('Bangladesh is my motherland.')
+f.close()
 
+# appending in a file
 f = open('demo.txt', 'a')
-f.write('I live in Bangladesh')
+f.write(' I love my country.')
+f.close()
+
+# open a file read only
+f = open('demo.txt', 'r')
+content = f.read()
 print(content)
 f.close()
 
-file = 'demo.txt'
-os.remove('demo.txt')
-
+# file delete
 try:
     os.remove('demo.txt')
-except:
-    print(file, 'does not exits')
+except FileNotFoundError:
+    print('File does not exits. Create the file first.')
+
+
+
+
+
